aa50ca5bd7ccc22ee0621548db566745bf23026a
tichise <ichise@gmail.com>
2015-05-09 20:31:45 +0900

    change class name

--- Sources/MaterialDesignIcon.swift
+++ Sources/MaterialDesignIcon.swift
@@ -1,11 +1,68 @@
 //
-//  MDIcon.swift
+//  MaterialDesignIcon
 //
 //  Created by tichise on 2015年5月7日 15/05/07.
 //  Copyright (c) 2015年 tichise. All rights reserved.
 //
 
 import UIKit
+
+@objc public class MaterialDesignIcon {
+    
+    var text:NSString = ""
+    var mutableTextFontAttributes = [NSObject : AnyObject]()
+    
+    init(text:NSString, size:CGFloat) {
+        self.text = text
+        
+        self.mutableTextFontAttributes = [NSObject : AnyObject]()
+        self.mutableTextFontAttributes[NSParagraphStyleAttributeName] = NSMutableParagraphStyle.defaultParagraphStyle().mutableCopy() as! NSMutableParagraphStyle
+        
+        if (UIFont.fontNamesForFamilyName("googleicon").count == 0) {
+            FontLoader.loadFont("googleicon")
+        }
+        
+        self.mutableTextFontAttributes[NSFontAttributeName] = UIFont(name: "googleicon" as String, size: size)!
+    }
+    
+    func addAttribute(attributeName:NSObject, value:AnyObject) {
+        self.mutableTextFontAttributes[attributeName as NSObject] = value
+    }
+    
+    func imageWithSize(size:CGSize)->UIImage {
+        let imageRect = CGRectMake(0,0,size.width, size.height)
+        
+        UIGraphicsBeginImageContextWithOptions(size, false, 0)
+        
+        let textRect  = CGRectMake(0, 0, size.width, size.height)
+        self.text.drawInRect(textRect, withAttributes: self.mutableTextFontAttributes)
+        
+        let image = UIGraphicsGetImageFromCurrentImageContext();
+        
+        UIGraphicsEndImageContext()
+        
+        return image
+    }
+}
+
+private class FontLoader {
+    class func loadFont(name: String) {
+        
+        let resourcePath:NSString = NSBundle.mainBundle().pathForResource("googleIconBundle", ofType:"bundle")!
+        let fileHandle : NSFileHandle = NSFileHandle(forReadingAtPath: resourcePath as String)!
+        let data : NSData = fileHandle.readDataToEndOfFile()
+        
+        let provider = CGDataProviderCreateWithCFData(data)
+        let font = CGFontCreateWithDataProvider(provider)!
+        
+        var error: Unmanaged<CFError>?
+        if !CTFontManagerRegisterGraphicsFont(font, &error) {
+            let errorDescription: CFStringRef = CFErrorCopyDescription(error!.takeUnretainedValue())
+            let nsError = error!.takeUnretainedValue() as AnyObject as! NSError
+            NSException(name: NSInternalInconsistencyException, reason: errorDescription as String, userInfo: [NSUnderlyingErrorKey: nsError]).raise()
+        }
+    }
+}
 
 @objc public class MDIcon {
     
@@ -1670,7 +1727,7 @@ import UIKit
     static let whatshot18px = "\u{ec7a}"
     static let whatshot24px = "\u{ec7b}"
     static let whatshot48px = "\u{ec7c}"
-
+    
     static let e600 = "\u{e600}"
     static let e601 = "\u{e601}"
     static let e602 = "\u{e602}"
