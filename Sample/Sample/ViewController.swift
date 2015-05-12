0009bbd5d712229a4602a814805e6955a4d0aee5
tichise <ichise@gmail.com>
2015-05-12 23:48:25 +0900

    add sample

--- Sample/Sample/ViewController.swift
+++ Sample/Sample/ViewController.swift
@@ -12,13 +12,17 @@ import MaterialDesignSymbol
 class ViewController: UIViewController {
     
     @IBOutlet var sampleImageView:UIImageView!
-
+    @IBOutlet var sampleLabel:UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         var symbol:MaterialDesignSymbol = MaterialDesignSymbol(text:MaterialDesignIcon.viewHeadline48px, size:30)
         var iconImage:UIImage = symbol.imageWithSize(CGSizeMake(30, 30))
         self.sampleImageView.image = iconImage
+
+        sampleLabel.font = MaterialDesignFont.fontOfSize(40)
+        sampleLabel.text = MaterialDesignIcon.clear48px
     }
 
     override func didReceiveMemoryWarning() {
