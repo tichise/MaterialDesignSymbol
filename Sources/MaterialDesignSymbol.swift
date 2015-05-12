cbd539de4b2c245ce71ff37ccb92508be5635d21
tichise <ichise@gmail.com>
2015-05-12 23:36:21 +0900

    update version 1.6

--- Sources/MaterialDesignSymbol.swift
+++ Sources/MaterialDesignSymbol.swift
@@ -62,9 +62,9 @@ private class FontLoader {
 }
 
 
-private class MaterialDesignFont {
+public class MaterialDesignFont {
     
-    class func fontOfSize(fontSize: CGFloat) -> UIFont {
+    public class func fontOfSize(fontSize: CGFloat) -> UIFont {
         
         struct Static {
             static var onceToken : dispatch_once_t = 0
