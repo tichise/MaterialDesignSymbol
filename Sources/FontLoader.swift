15303076ae34406bac142807ade126f12747ce58
takuya ichise <ichise@gmail.com>
2016-07-08 18:38:38 +0900

    split class

---
+++ Sources/FontLoader.swift
@@ -0,0 +1,35 @@
+//
+//  FontLoader
+//
+//  Created by tichise on 2015/5/7 15/05/07.
+//  Copyright (c) 2015 tichise. All rights reserved.
+//
+
+import UIKit
+
+/// フォント読み込み用クラス
+public class FontLoader {
+    
+    /**
+     引数で渡ってきたフォントを読み込みます
+     - parameter name: フォントファイル名
+     */
+    public class func loadFont(name: String) {
+        
+        let ttfPath = NSBundle(forClass:object_getClass(self)).pathForResource(name, ofType:"ttf")
+        
+        let fileHandle : NSFileHandle = NSFileHandle(forReadingAtPath: ttfPath!)!
+        let data : NSData = fileHandle.readDataToEndOfFile()
+        
+        let provider = CGDataProviderCreateWithCFData(data)
+        let font = CGFontCreateWithDataProvider(provider)!
+        
+        var error: Unmanaged<CFError>?
+        
+        if !CTFontManagerRegisterGraphicsFont(font, &error) {
+            let errorDescription: CFStringRef = CFErrorCopyDescription(error!.takeUnretainedValue())
+            let nsError = error!.takeUnretainedValue() as AnyObject as! NSError
+            NSException(name: NSInternalInconsistencyException, reason: errorDescription as String, userInfo: [NSUnderlyingErrorKey: nsError]).raise()
+        }
+    }
+}
---
+++ Sources/MaterialDesignFont.swift
@@ -0,0 +1,37 @@
+//
+//  MaterialDesignFont
+//
+//  Created by tichise on 2015/5/7 15/05/07.
+//  Copyright (c) 2015 tichise. All rights reserved.
+//
+
+import UIKit
+
+public class MaterialDesignFont {
+    
+    /**
+     アイコンをフォント形式で呼び出すのに使うメソッド
+     - parameter fontSize: フォントサイズ
+     - returns: UIFont
+     */
+    public class func fontOfSize(fontSize: CGFloat) -> UIFont {
+        
+        struct Static {
+            
+            // MARK: - Properties
+            static var onceToken : dispatch_once_t = 0
+        }
+        
+        /// 呼び出すアイコンファイル名
+        let name = "material-design-icons"
+        
+        // アイコンを呼び出す
+        if (UIFont.fontNamesForFamilyName(name).count == 0) {
+            dispatch_once(&Static.onceToken) {
+                FontLoader.loadFont(name)
+            }
+        }
+        
+        return UIFont(name: name, size: fontSize)!
+    }
+}
