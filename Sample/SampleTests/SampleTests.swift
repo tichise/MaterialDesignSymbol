f19d30bcc08fedc913ea19914c1c31b1df060520
tichise <ichise@gmail.com>
2016-07-08 21:34:56 +0900

    add test

--- Sample/SampleTests/SampleTests.swift
+++ Sample/SampleTests/SampleTests.swift
@@ -8,29 +8,33 @@
 
 import UIKit
 import XCTest
+import MaterialDesignSymbol
 
 class SampleTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
+    func testImage() {
+        
+        let symbol:MaterialDesignSymbol = MaterialDesignSymbol(text:MaterialDesignIcon.list48px, size:25)
+        symbol.addAttribute(NSForegroundColorAttributeName, value: UIColor.redColor())
+        let iconImage:UIImage = symbol.imageWithSize(CGSizeMake(25, 25))
+        
+        XCTAssertTrue(iconImage.size.width > 0 && iconImage.size.height > 0)
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock() {
-            // Put the code you want to measure the time of here.
-        }
+    func testFont() {
+        let titleLabel:UILabel = UILabel.init()
+        titleLabel.font = MaterialDesignFont.fontOfSize(20)
+        titleLabel.text = MaterialDesignIcon.list48px
+        
+        XCTAssertTrue(titleLabel.text?.characters.count == 1)
     }
     
 }
