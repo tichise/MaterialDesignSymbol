bf46ce3fbf58f96610661a589d859449d074feea
tichise <43707+tichise@users.noreply.github.com>
2020-08-10 23:17:08 +0900

    add SampleView

---
+++ Sample/Sample/SampleView.swift
@@ -0,0 +1,30 @@
+//
+//  SampleView.swift
+//  Sample
+//
+//  Created by tichise on 2020年8月10日 20/08/10.
+//  Copyright © 2020 tichise. All rights reserved.
+//
+
+import SwiftUI
+import MaterialDesignSymbol
+
+@available(iOS 13.0, *)
+/*
+extension Image {
+    init(icon: MaterialDesignIcon,  size: CGFloat) {
+        let symbol = MaterialDesignSymbol(text:  icon.raw, size: size)
+        symbol.addAttribute(attributeName: NSAttributedString.Key.foregroundColor, value: UIColor.black)
+        
+        let iconImage = symbol.image(size: CGSize(width: size, height: size))
+        
+        self.init(uiImage: iconImage)
+    }
+}
+
+struct ImageSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        Image(iconText: MaterialDesignIcon.lens24px, size: 100)
+    }
+}
+*/
