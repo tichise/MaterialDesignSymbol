79bc951e334495a1ef1a3958fb7aad9c959a90a2
tichise <ichise@gmail.com>
2020-08-10 13:05:49 +0900

    refactoring

--- Sources/FontLoader.swift
+++ Sources/FontLoader.swift
@@ -14,24 +14,37 @@ open class FontLoader {
     
     /**
      引数で渡ってきたフォントを読み込みます
-     
      - parameter name: フォントファイル名
      */
     open class func loadFont(_ name: String) {
         
-        let ttfPath = Bundle(for: object_getClass(self)!).path(forResource: name, ofType:"ttf")
+        guard let ttfPath = Bundle(for: object_getClass(self)!).path(forResource: name, ofType: "ttf") else {
+            fatalError("Can't find the ttfPath.")
+        }
         
-        let fileHandle = FileHandle(forReadingAtPath: ttfPath!)!
+        guard let fileHandle = FileHandle(forReadingAtPath: ttfPath) else {
+            fatalError("Can't find the fileHandle.")
+        }
+
         let data = fileHandle.readDataToEndOfFile()
         
-        let provider = CGDataProvider(data: data as CFData)
-        let font = CGFont(provider!)
+        guard let provider = CGDataProvider(data: data as CFData) else {
+            fatalError("Can't find the provider.")
+        }
+
+        guard let font = CGFont(provider) else {
+            fatalError("Can't find the font.")
+        }
         
         var error: Unmanaged<CFError>?
         
-        if !CTFontManagerRegisterGraphicsFont(font!, &error) {
-            let errorDescription: CFString = CFErrorCopyDescription(error!.takeUnretainedValue())
-            let nsError = error!.takeUnretainedValue() as AnyObject as! NSError
+        if !CTFontManagerRegisterGraphicsFont(font, &error) {
+            guard let error = error else {
+                fatalError("Error not found.")
+            }
+
+            let errorDescription: CFString = CFErrorCopyDescription(error.takeUnretainedValue())
+            let nsError = error.takeUnretainedValue() as AnyObject as! NSError
             
             NSException(name: NSExceptionName.internalInconsistencyException, reason: errorDescription as String, userInfo: [NSUnderlyingErrorKey: nsError]).raise()
         }
