c75f147fb3bf81d758b5ef0a44c218d2fb396e44
tichise <ichise@gmail.com>
2020-08-10 15:54:11 +0900

    refactoring

--- Sources/FontLoader.swift
+++ Sources/FontLoader.swift
@@ -10,43 +10,43 @@ import UIKit
 /**
  フォント読み込み用クラス
  */
-open class FontLoader {
-
+public class FontLoader {
+    
     /**
      引数で渡ってきたフォントを読み込みます
      - parameter name: フォントファイル名
      */
-    open class func loadFont(_ name: String) {
+    public class func loadFont(_ name: String) throws {
 
         guard let ttfPath = Bundle(for: object_getClass(self)!).path(forResource: name, ofType: "ttf") else {
-            fatalError("Can't find the ttfPath.")
+            throw FontError.invalidFontFile
         }
 
         guard let fileHandle = FileHandle(forReadingAtPath: ttfPath) else {
-            fatalError("Can't find the fileHandle.")
+            throw FontError.fontPathNotFound
         }
 
         let data = fileHandle.readDataToEndOfFile()
 
         guard let provider = CGDataProvider(data: data as CFData) else {
-            fatalError("Can't find the provider.")
+            throw FontError.invalidFontFile
         }
 
         guard let font = CGFont(provider) else {
-            fatalError("Can't find the font.")
+            throw FontError.initFontError
         }
 
         var error: Unmanaged<CFError>?
 
         if !CTFontManagerRegisterGraphicsFont(font, &error) {
-            guard let error = error else {
-                fatalError("Error not found.")
-            }
-
-            let errorDescription: CFString = CFErrorCopyDescription(error.takeUnretainedValue())
-            let nsError = error.takeUnretainedValue() as AnyObject as! NSError
-
-            NSException(name: NSExceptionName.internalInconsistencyException, reason: errorDescription as String, userInfo: [NSUnderlyingErrorKey: nsError]).raise()
+            throw FontError.registerFailed
         }
     }
+}
+
+public enum FontError: Error {
+  case invalidFontFile
+  case fontPathNotFound
+  case initFontError
+  case registerFailed
 }
